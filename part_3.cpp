870fdedc983bba3b76ec57c7ced3aff2def510f1
vijaypmk <vijay.pmk11@gmail.com>
2016-11-24 15:30:52 -0800

    Changed name to part_3

---
+++ part_3.cpp
@@ -0,0 +1,195 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp" 
+#include "opencv2/imgproc/imgproc.hpp" 
+#include "opencv2/calib3d/calib3d.hpp"
+#include <string.h>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+
+void callback1_Func(int event, int x, int y, int flags, void* userdate);
+void callback2_Func(int event, int x, int y, int flags, void* userdate);
+
+class Part_3
+{
+
+    public:
+    // members
+    vector<Point2f> pos1,pos2;
+    Mat img1, img2, undist_img1, undist_img2;
+    double fx, fy, cx, cy;
+    Mat dc, E, F, mask;
+
+    // constructor
+    Part_3()
+    {
+        dc = Mat::eye(5, 1, CV_64F);
+    }
+
+    // member functions
+    void init_image(string a, string b)
+    {
+        img1 = imread(a);
+        img2 = imread(b);
+    }
+
+    void display_image(Mat i1, Mat i2)
+    {
+        namedWindow("image1", WINDOW_NORMAL);
+        namedWindow("image2", WINDOW_NORMAL);
+
+        imshow("image1", i1);
+        imshow("image2", i2);
+
+        // set the callback function for any mouse event
+        setMouseCallback("image1", callback1_Func, NULL);
+        setMouseCallback("image2", callback2_Func, NULL);
+
+        // Wait until user press some key
+         waitKey(0);
+    }
+
+    void undistort_image()
+    {
+        string filename = "camera.yml";
+        FileStorage fs(filename, FileStorage::READ);
+
+        // double fx, fy, cx, cy;
+        FileNode n = fs["camera_matrix"];
+        FileNode ns = n["data"];
+        fx = (double) ns[0];
+        fy = (double) ns[4];
+        cx = (double) ns[2];
+        cy = (double) ns[5];
+
+        FileNode d = fs["distortion_coefficients"];
+        FileNode ds = d["data"];
+
+        // distortion coefficient vector
+        int k = 0;
+
+        for(int r=0;r<5;r++)
+            for(int q=0;q<1;q++)
+            {
+                dc.at<double>(r, q) = (double) ds[k];
+                k++;
+            }
+
+
+        Mat lr, dt1, dt2, cam_mat = Mat::eye(3, 3, CV_64F);
+
+        // camera matrix
+        k = 0;
+        for(int r=0;r<3;r++)
+        {
+            for(int q=0;q<3;q++)
+            {
+                cam_mat.at<double>(r, q) = (double) ns[k];
+                k++;
+            }
+        }
+
+        dt1 = getOptimalNewCameraMatrix(cam_mat, dc, img1.size(), 1.0, img1.size());
+        dt2 = getOptimalNewCameraMatrix(cam_mat, dc, img2.size(), 1.0, img2.size());
+
+        undist_img1 = img1.clone();
+        undist_img2 = img2.clone();
+        
+        undistort(img1, undist_img1, dt1, dc);
+        undistort(img2, undist_img2, dt2, dc);
+
+        display_image(undist_img1, undist_img2);
+    }
+
+    void epipolar_image()
+    {
+        Point2d pp(cx, cy);
+        E = findEssentialMat(pos1, pos2, fx, pp, RANSAC, 0.999, 1.0, mask);
+        F = findFundamentalMat(pos1, pos2, CV_FM_RANSAC, 0.999, 1.0, mask);
+        cout << "E "<< E <<endl;
+        cout << "F "<< F <<endl;
+    }
+
+}p;
+
+// mouse callback function
+void callback1_Func(int event, int x, int y, int flags, void* userdate)
+{
+    if (event == EVENT_LBUTTONDOWN )
+    {
+        static int i=0;
+        if (i >= 20) 
+        {
+            cout << "Too many points clicked." <<endl;
+            return;
+        }
+        cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;        
+        p.pos1.push_back(Point(x, y));
+        i++;
+        namedWindow("image1", WINDOW_NORMAL);
+        rectangle(p.undist_img1, Point(x-50, y-50), Point(x+50, y+50), Scalar(0,255,0), -1);
+        imshow("image1", p.undist_img1);
+        waitKey(0);
+    }
+}
+
+void callback2_Func(int event, int x, int y, int flags, void* userdate)
+{
+    if (event == EVENT_LBUTTONDOWN )
+    {   static int j=0;
+        if (j >= 20) 
+        {
+            cout << "Too many points clicked." <<endl;
+            return;
+        }
+        cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;        
+        p.pos2.push_back(Point(x, y));
+        j++;
+        rectangle(p.undist_img2, Point(x-50, y-50), Point(x+50, y+50), Scalar(0,255,0), -1);
+        namedWindow("image2",WINDOW_NORMAL);
+        imshow("image2", p.undist_img2);
+        waitKey(0);
+    }
+}
+
+
+//template <typename T1, typename T2>
+int main(int argc, char** argv)
+{
+    string a, b;
+    a = "IMG_L.JPG";
+    b = "IMG_C.JPG";
+    p.init_image(a, b);
+    p.undistort_image();
+    p.epipolar_image();
+    
+    //// vector<Vec4i> epilines1, epilines2;
+    //
+    ////computeCorrespondEpilines(pos1, 1, E, epilines1);
+    ////computeCorrespondEpilines(pos2, 2, E, epilines2);
+    ////lr = E*lr;
+
+    ////for(int m=0;m<E.cols;m++) 
+    ////{ 
+    ////    for(int n=0;n<E.rows;n++) 
+    ////    { 
+    ////        lr[m][n] = E[m][n]*pos1[n]; 
+    ////    } 
+    ////}
+
+    ////RNG rng(0);
+    ////Scalar color(rng(256),rng(256),rng(256));
+
+    ////CV_Assert(pos1.size() == pos2.size() &&
+    ////        pos2.size() == epilines1.size() &&
+    ////        epilines1.size() == epilines2.size());
+
+    ////line(img2,
+    ////Point(0,-epilines1[i][2]/epilines1[i][1]),
+    ////Point(img1.cols,-(epilines1[i][2]+epilines1[i][0]*img1.cols)/epilines1[i][1]),
+    ////    color);
+
+    return 0;
+    
+}
